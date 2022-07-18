f7ce184120c8874d5fa7645058fc7ac8883ad42f
Caleb Woodbine <calebwoodbine.public@gmail.com>
2022-07-18 15:57:08 +1200

    Update comments on accounts provider assume role
    Signed-off-by: Caleb Woodbine <calebwoodbine.public@gmail.com>

--- terraform/iam/providers.tf
+++ terraform/iam/providers.tf
@@ -35,10 +35,9 @@ provider "aws" {
 
   region = "us-west-1"
 
-  # Jay, Hippie and Caleb are cool
   assume_role {
     # deleting this role causes bad things. You just really don't want this. Count this as a WARNING!
-    # also! the account (513428760722) is CNCF/Kubernetes/registry.k8s.io/registry.k8s.io_admin / k8s-infra-aws-registry-k8s-io-admin@kubernetes.io
+    # also! the account (585803375430) is CNCF/Kubernetes/k8s-infra-accounts / k8s-infra-accounts@kubernetes.io
     role_arn = "arn:aws:iam::585803375430:role/OrganizationAccountAccessRole"
   }
 }
